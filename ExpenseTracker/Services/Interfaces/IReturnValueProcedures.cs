945df7ec23bf110198923418d76798b90743db18
doks-coders <guonnie@gmail.com>
2024-03-19 10:14:31 +0100

    Stage 2 | Add Comments

--- ExpenseTracker/Services/Interfaces/IReturnValueProcedures.cs
+++ ExpenseTracker/Services/Interfaces/IReturnValueProcedures.cs
@@ -4,14 +4,39 @@ namespace ExpenseTracker.Services.Interfaces
 {
     public interface IReturnValueProcedures
     {
+        /// <summary>
+        /// This method gets the total number of all the user's expenses
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
         Task<TotalExpense> GetTotalUserExpenses(string userName);
 
+        /// <summary>
+        /// This method gets the particular expense using the expenseId
+        /// </summary>
+        /// <param name="expenseId"></param>
+        /// <returns></returns>
         Task<Expenses?> GetExpenseFromId(int expenseId);
 
+        /// <summary>
+        /// This method gets all the expenses for a particular category
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="categoryName"></param>
+        /// <returns></returns>
         Task<List<ExpenseForCategory>> GetExpensesForCategory(int pageNumber, string categoryName);
 
+        /// <summary>
+        /// This method gets the expenses associated with the user
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="userName"></param>
+        /// <returns></returns>
         Task<List<UserExpenses>> GetUserExpenses(int pageNumber, string userName);
 
+        /// <summary>
+        /// This method gets the total cost of all the user's expenses
+        /// </summary>
         Task<TotalCost> GetTotalCosts(string username);
 
 
--- ExpenseTracker/Services/Interfaces/ISetValueProcedures.cs
+++ ExpenseTracker/Services/Interfaces/ISetValueProcedures.cs
@@ -4,10 +4,25 @@ namespace ExpenseTracker.Services.Interfaces
 {
     public interface ISetValueProcedures
     {
+        /// <summary>
+        /// This method deletes the expense from the database
+        /// </summary>
+        /// <param name="expenseId"></param>
+        /// <returns></returns>
         Task<bool> DeleteExpense(int expenseId);
 
+        /// <summary>
+        /// This method edits the expense in the database
+        /// </summary>
+        /// <param name="editExpense"></param>
+        /// <returns></returns>
         Task<bool> EditExpense(EditExpenseRequest editExpense);
 
+        /// <summary>
+        /// This method uploads the expense in the database
+        /// </summary>
+        /// <param name="uploadExpense"></param>
+        /// <returns></returns>
         Task<bool> UploadExpense(UploadExpenseRequest uploadExpense);
 
     }
